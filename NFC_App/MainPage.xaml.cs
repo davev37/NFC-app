7c6613925b5385b0d33c28fa6cdb452d184ac68f
hilverdinkjulian <100442756+hilverdinkjulian@users.noreply.github.com>
2022-10-17 11:27:22 +0200

    Add files via upload
    Deez

---
+++ NFC_App/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NFC_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ToSecondPage()
+        {
+            Navigation.PushAsync(new Register());
+        }
+
+        public void GoToSecondPage(object Sender, EventArgs e)
+        {
+            ToSecondPage();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NFC_App/Register.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NFC_App
+{
+    
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        public void GoToLoginPage(object Sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
